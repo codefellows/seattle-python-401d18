86a869bb006526eff1222e6eeb42623e1857d21f
JB-Tellez <jb.tellez@gmail.com>
2022-04-21 07:22:40 -0700

    tweaks dog pack demo for clarity

--- class-04/demo/dog-pack/dog_pack/dogs.py
+++ class-04/demo/dog-pack/dog_pack/dogs.py
@@ -1,45 +1,49 @@
-from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
+import abc
+from abc import ABC
 
 
 class Dog(ABC):
-
-    count = 0
+    all_dog_count = 0
+    per_breed_count = 0
 
     def __init__(self, name="unknown"):
         self.name = name
-        self.__class__.increase_count()
+        self.__class__.per_breed_count += 1
+        Dog.all_dog_count += 1
+
+    @abc.abstractmethod
+    def greet(self):
+        pass
 
     def sleep(self):
         return "zzz"
 
-    @abstractclassmethod
-    def increase_count(cls):
-        Dog.count += 1
-
-    @abstractmethod
-    def greet(self):
-        pass
-
-    @abstractstaticmethod
-    def get_characteristics():
-        pass
-
     @classmethod
     def get_all_dog_count(cls):
-        return cls.count
+        return cls.all_dog_count
+
+    @classmethod
+    def get_breed_count(cls):
+        return cls.per_breed_count
+
+
+class Puggle(Dog):
+
+    def __str__(self):
+        return f"Puggle named {self.name}"
+
+    def greet(self):
+        return f"I am {self.name}. I am SO HAPPY to meet you!"
+
+    @staticmethod
+    def get_characteristics():
+        return "Like a mini boxer"
 
 
 class Boxer(Dog):
-    """
-    A super cool dog
-    """
 
-    count = 0
-
-    @classmethod
-    def increase_count(cls):
-        cls.count += 1
-        return super().increase_count()
+    def __str__(self):
+        return f"Boxer named {self.name}"
 
     def greet(self):
         return f"The name's {self.name}. Pleasure."
@@ -47,31 +51,3 @@ class Boxer(Dog):
     @staticmethod
     def get_characteristics():
         return "Boxers are lovers, not fighters."
-
-    @classmethod
-    def get_breed_count(cls):
-        return cls.count
-
-
-class Puggle(Dog):
-    """
-    adorably spunky
-    """
-
-    count = 0
-
-    def greet(self):
-        return f"I am {self.name}. I am SO HAPPY to meet you!"
-
-    @classmethod
-    def increase_count(cls):
-        cls.count += 1
-        return super().increase_count()
-
-    @staticmethod
-    def get_characteristics():
-        return "Like a mini boxer"
-
-    @classmethod
-    def get_breed_count(cls):
-        return cls.count
