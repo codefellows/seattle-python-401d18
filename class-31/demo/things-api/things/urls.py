7660c07fe0be36c41fc59e63a7526b36634ffb23
JB-Tellez <jb.tellez@gmail.com>
2022-05-26 12:15:40 -0700

    adds in class changes and Docker stuff

--- class-31/demo/things-api/things/urls.py
+++ class-31/demo/things-api/things/urls.py
@@ -3,5 +3,5 @@ from .views import ThingList, ThingDetail
 
 urlpatterns = [
     path("", ThingList.as_view(), name="thing_list"),
-    path("<int:pk>/", ThingDetail.as_view(), name="thing_detail"),
+    path("<int:pk>/", ThingDetail.as_view(), name="thing_detail")
 ]
