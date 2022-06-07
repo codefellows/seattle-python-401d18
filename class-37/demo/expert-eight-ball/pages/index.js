cfc63170a100bbe0d94f610a0ab5022df38d54d4
JB-Tellez <jb.tellez@gmail.com>
2022-06-06 22:32:23 -0700

    updates expert eight ball demo to latest version of next

--- class-37/demo/expert-eight-ball/pages/index.js
+++ class-37/demo/expert-eight-ball/pages/index.js
@@ -8,9 +8,14 @@ export default function Home() {
 
     function questionAskedHandler(event) {
         event.preventDefault();
-        const randomReply = Math.random() > .5 ? "Yes" : "No";
+        let randomReply = Math.random() > .5 ? "Yes" : "No";
+        if (event.target.question.value.toLowerCase().includes("snickers")) {
+            randomReply = "Yes";
+        }
+
         setQuestion(event.target.question.value);
         setReply(randomReply)
+        event.target.reset()
     }
     return (
         <div>
@@ -18,46 +23,46 @@ export default function Home() {
                 <title>Expert Eight Ball</title>
             </Head>
             <Header />
-            <main className="my-4">
+            <main className="flex flex-col items-center py-4 space-y-8">
                 <QuestionForm onSubmit={questionAskedHandler} />
                 <EightBall question={question} />
                 <Response reply={reply} />
             </main>
-            <Footer copyright="2021" />
+            <Footer copyright="2022" />
         </div>
     )
 }
 
 function EightBall({ question }) {
-    return <div className="relative mx-auto my-8 bg-gray-900 rounded-full h-96 w-96">
-        <div className="absolute flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
+    return <div className="bg-gray-900 rounded-full w-96 h-96">
+        <div className="relative flex items-center justify-center w-56 h-56 rounded-full bg-gray-50 left-12 top-12">
             <p>{question}</p>
         </div>
     </div>
 }
 
 function Header() {
-    return <header className="px-8 py-6 bg-gray-500">
-        <h1 className="text-4xl text-white">Expert 8 Ball</h1>
+    return <header className="px-8 py-6 text-4xl bg-gray-500 text-gray-50">
+        <h1>Expert 8 Ball</h1>
     </header>
 }
 
 function QuestionForm(props) {
     return (
-        <form onSubmit={props.onSubmit} className="flex w-1/2 p-2 mx-auto bg-gray-200">
-            <input name="question" className="flex-auto pl-2" />
+        <form onSubmit={props.onSubmit} className="flex w-1/2 p-2 bg-gray-200">
+            <input name="question" className="flex-auto pl-2" placeholder='Ask me anything...' required />
             <button className="px-4 py-2 bg-gray-400 text-gray-50">Ask</button>
         </form>
     )
 }
 
 function Response({ reply }) {
-    return <p className="text-center">{reply}</p>
+    return <p className="">{reply}</p>
 }
 
 function Footer({ copyright }) {
     return (
-        <footer className="px-8 py-6 text-white bg-gray-500">
+        <footer className="px-8 py-6 bg-gray-500 text-gray-50">
             <p>&copy;{copyright}</p>
         </footer>
     )
